bc21134b137f41a82618838318c2cc0cd41a14ba
Lakshmiprasad <slakshmiprasad93@gmail.com>
2017-11-25 23:34:37 +0530

    feat: store the token in window localStorage

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -34,3 +34,19 @@ angular.module('authService', [])
     	}
     }
 })
+
+.factory('AuthToken', function($window) {
+    var authTokenFactory = {};
+
+    authTokenFactory.getToken = function() {
+        return $window.localStorage.getItem('token');
+    }
+
+    authTokenFactory.setToken = function(token) {
+        if(token){
+            $window.localStorage.setItem('token', token);
+        }else{
+            $window.localStorage.removeItem('token');
+        }
+    }
+})
