8d36b48f4612493653230692856db4bc45248aea
Lakshmiprasad <slakshmiprasad93@gmail.com>
2017-12-04 23:54:18 +0530

    feat: login html

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -3,7 +3,7 @@ angular.module('mainCtrl', [])
 .controller('MainController', function($rootScope, $location, Auth) {
 
     var vm = this;
-    vm.loggedIn = Auth.isLogged();
+    vm.loggedIn = Auth.isLoggedIn();
     $rootScope.$on('$routeChangeStart', function(){
         vm.loggedIn = Auth.isLoggedIn();
         Auth.getUser()
@@ -29,11 +29,11 @@ angular.module('mainCtrl', [])
                     $location.path('/');
                 }
             });
-    }
+    };
 
     vm.doLogout = function(){
         Auth.logout();
         $location.path('/logout');
-    }
+    };
     
 })
